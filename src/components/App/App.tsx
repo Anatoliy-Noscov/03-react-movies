cedeee77823671a36ee9ed91a1a6a30eaea03684
Anatoliy_Noscov <svodnuy7777@gmail.com>
2025-05-29 05:36:01 +0300

    #40

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import css from "./App.module.css";
-import SearchBar from "../SearchBar/SearchBar";
+import SearchBar from "../../components/SearchBar/SearchBar";
 import { type Movie } from "../../types/movie";
-import MovieGrid from "../MovieGrid/MovieGrid";
+import MovieGrid from "../../components/MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
