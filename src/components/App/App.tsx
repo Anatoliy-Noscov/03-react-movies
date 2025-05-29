ce343edcf6868c48e1765c4083611315a1e73403
Anatoliy_Noscov <svodnuy7777@gmail.com>
2025-05-29 06:04:33 +0300

    #42

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import css from "./App.module.css";
-import SearchBar from "components/SearchBar/SearchBar";
+import SearchBar from "../SearchBar/SearchBar";
 import { type Movie } from "../../types/movie";
-import MovieGrid from "components/MovieGrid/MovieGrid";
-import Loader from "components/Loader/Loader";
-import ErrorMessage from "components/ErrorMessage/ErrorMessage";
-import MovieModal from "components/MovieModal/MovieModal";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
 import { getMovies } from "../../services/movieService";
 import toast from "react-hot-toast";
 import { Toaster } from "react-hot-toast";
